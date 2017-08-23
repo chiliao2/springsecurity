2f4b0f47a33e66b1442a4077b2c8072d51ccc85e
chiliao2 <553186538@qq.com>
2017-08-23 17:49:10 +0800

    增加注解

--- src/main/java/com/cdz/jn/repository/RoleRepository.java
+++ src/main/java/com/cdz/jn/repository/RoleRepository.java
@@ -2,6 +2,8 @@ package com.cdz.jn.repository;
 
 import com.cdz.jn.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
+@NoRepositoryBean
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 }
--- src/main/java/com/cdz/jn/repository/UserRepository.java
+++ src/main/java/com/cdz/jn/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.cdz.jn.repository;
 
 import com.cdz.jn.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
 /**
  * 访问数据接口
  */
+@NoRepositoryBean
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUsername(String username);
 }
